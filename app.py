5bd04be9020aa0098e4203e4a524fd6a7c9d50e4
IvanDanielEscobar <i.escobar@itecriocuarto.org.ar>
2025-07-01 15:58:02 -0300

    armandio flask

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import (
+    Flask,
+    render_template
+    )
+
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.secret_key = "cualquiercosa"
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    "mysql+pymysql://root:@localhost/miniblog"
+)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+@app.route('/')
+def miniblog():
+    return render_template('index.html')
+
+@app.route('/user')
+def userconf():
+    return render_template('userconfig.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
